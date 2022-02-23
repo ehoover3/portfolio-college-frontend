57d8b603388b42cfc38ffaef6b275d5d80fb707b
Eric Hoover <ehoover3@yahoo.com>
2022-02-23 12:40:35 -0800

    first commit

---
+++ code.js
@@ -0,0 +1,92 @@
+// 1. get user coordinates via geolocation api
+async function getCoords() {
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+  return [pos.coords.latitude, pos.coords.longitude];
+}
+
+// 2. get school data
+async function getSchoolsData(querySchoolName) {
+  let urlPath = "https://api.data.gov/ed/collegescorecard/v1";
+  let name = querySchoolName.replace(" ", "%");
+  let apiKey = "b5LgbbLogx9cQTekEa8M5ZQ0lpd52c4DdPS8BOca";
+  let response = await fetch(`${urlPath}/schools?school.name=${name}&api_key=${apiKey}`);
+  let schoolsData = await response.json();
+  return schoolsData;
+}
+
+// 3. process school data
+function processSchoolsData(dataObject) {
+  let dataArray = dataObject.results;
+  // *** temporary fix ---> removes schools whose latitude is null ***
+  let filteredDataArray = dataArray.filter((school) => school.location.lat != null);
+  // *** end temporary fix ***
+  let schools = filteredDataArray.map((school) => {
+    let schoolData = {
+      name: school.school.name,
+      lat: school.location.lat,
+      long: school.location.lon,
+    };
+
+    return schoolData;
+  });
+  return schools;
+}
+
+// 4. map object
+const myMap = {
+  coordinates: [],
+  map: {},
+  schools: [],
+  markers: {},
+};
+
+// 5. build map
+function buildMap() {
+  myMap.map = L.map("map", {
+    center: myMap.coordinates,
+    zoom: 11,
+  });
+  // add openstreetmap tiles
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    // minZoom: "15",
+  }).addTo(myMap.map);
+  // create and add geolocation marker
+  const marker = L.marker(myMap.coordinates);
+  marker.addTo(myMap.map).bindPopup("<p1><b>You are here</b><br></p1>").openPopup();
+}
+
+// 6. add markers
+function addMarkers() {
+  for (let i = 0; i < myMap.schools.length; i++) {
+    myMap.markers = L.marker([myMap.schools[i].lat, myMap.schools[i].long])
+      .bindPopup(`<p1>${myMap.schools[i].name}</p1>`)
+      .addTo(myMap.map);
+  }
+  console.log(myMap.schools);
+}
+
+// event - window load
+window.onload = async () => {
+  const coords = await getCoords();
+  myMap.coordinates = coords;
+  buildMap();
+};
+
+// event - submit button
+document.getElementById("submit").addEventListener("click", async (event) => {
+  event.preventDefault();
+  let business = document.getElementById("business").value;
+  // let data = await getFoursquare(business);
+  let data = await getSchoolsData(business);
+  myMap.schools = processSchoolsData(data);
+  addMarkers();
+
+  console.log(myMap.coordinates);
+  console.log(myMap.map);
+  console.log(myMap.schools);
+  console.log(myMap.markers);
+});
---
+++ readme.md
@@ -0,0 +1,23 @@
+# The Problem
+
+Asymmetric information, also known as "information failure," occurs when one party to an economic transaction possesses greater material knowledge than the other party.
+
+Many 18 year old college students frequently choose a college, but may not know that choosing to enroll in a comparable college could save them $50,000 or more.
+
+# The Solution
+
+This program allows students to input a school search, which then shows:
+
+- an interactive map showing school locations
+- a list that compares schools cost
+- a field that shows the estimated savings between 2 or more colleges
+
+# Documentation
+
+- College Scorecard Data API
+- https://github.com/RTICWDT/open-data-maker/blob/master/API.md
+
+- Leaflet Library
+- https://leafletjs.com/
+# portfolio-college-frontend
+# portfolio-college-frontend
